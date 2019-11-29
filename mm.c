8f5e8fbaef17f0bbbf6bc595b3479dee2d96d1f6
twan <wang_cool2008@hotmail.com>
2019-11-29 19:50:18 +0100

    consistency

--- mm.c
+++ mm.c
@@ -132,9 +132,9 @@ static void *extend_heap(size_t words)
         return NULL;
 
     /* Initialize free block header/footer and the epilogue header */
-    PUT(HDRP(bp), PACK(size, 0));       /* Free block header */
-    PUT(FTRP(bp), PACK(size, 0));       /* Free block footer */
-    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1)); /* New epilogue header */
+    PUT(HEADER(bp), PACK(size, 0));       /* Free block header */
+    PUT(FOOTER(bp), PACK(size, 0));       /* Free block footer */
+    PUT(HEADER(NEXT_BLKP(bp)), PACK(0, 1)); /* New epilogue header */
 
     /* Coalesce if the previous block was free */
     return coalesce(bp);
